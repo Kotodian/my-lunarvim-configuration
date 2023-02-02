d3ee5a3c93da63196400f933ba256e9c985c6bf5
linqiankai <417350372@qq.com>
2023-02-02 16:41:51 +0800

    新增插件

--- config.lua
+++ config.lua
@@ -1,39 +1,45 @@
 --[[
- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
- `lvim` is the global options object
-]]
-
--- vim options
+THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+ `lvim` is the global options object ]] -- vim options
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
 
 -- general
 lvim.log.level = "info"
+
 lvim.format_on_save = {
     enabled = true,
-    pattern = "*.lua,*.rs,*.go",
+    pattern = "*.lua,*.rs,*.go,*.proto",
     timeout = 1000,
 }
-lvim.colorscheme = "slate"
-lvim.transparent_window = true
 -- lvim.use_icons = false
-
--- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
-lvim.leader = "space"
+-- keymappings <https://www.lunarvim.org/docs/configuration/keybindings> lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
-lvim.keys.normal_mode["<C-g>"] = ":lua toggle_lazygit()<cr>"
+lvim.keys.normal_mode["<S-n>"] = ":NvimTreeToggle<CR>"
+
 -- -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+-- 	name = "Trouble",
+-- 	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+-- 	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+-- 	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+-- 	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+-- 	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+-- 	w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+-- }
 
 -- -- Change theme settings
--- lvim.colorscheme = "lunar"
+lvim.colorscheme = "slate"
+lvim.transparent_window = true
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerSync
 lvim.builtin.alpha.active = true
@@ -45,17 +51,21 @@ lvim.builtin.gitsigns.opts.current_line_blame = true
 
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
+lvim.builtin.treesitter.ensure_installed = {
+    "rust",
+    "go",
+    "gomod",
+}
 
 -- lvim.builtin.treesitter.ignore_install = { "haskell" }
 
 -- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
-
 -- --- disable automatic installation of servers
 -- lvim.lsp.installer.setup.automatic_installation = false
-
+require 'lspconfig'.bufls.setup {}
 -- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
 -- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
 -- require("lvim.lsp.manager").setup("pyright", opts)
 
@@ -76,15 +86,6 @@ lvim.builtin.treesitter.auto_install = true
 -- end
 
 -- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-width", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
 -- local linters = require "lvim.lsp.null-ls.linters"
 -- linters.setup {
 --   { command = "flake8", filetypes = { "python" } },
@@ -93,20 +94,6 @@ lvim.builtin.treesitter.auto_install = true
 --     args = { "--severity", "warning" },
 --   },
 -- }
-
--- lazygit
-local _lazygit = nil
-_G.toggle_lazygit = function()
-    if not _lazygit then
-        local Terminal = require("toggleterm.terminal").Terminal
-        _lazygit = Terminal:new({
-            cmd = "lazygit",
-            hidden = true,
-            direction = "float",
-        })
-    end
-    _lazygit:toggle()
-end
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 
@@ -135,20 +122,20 @@ lvim.plugins = {
     {
         "rmagatti/goto-preview",
         config = function()
-            require('goto-preview').setup {
-                width = 120; -- Width of the floating window
-                height = 25; -- Height of the floating window
-                default_mappings = false; -- Bind default mappings
-                debug = false; -- Print debug information
-                opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
-                post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
+            require("goto-preview").setup({
+                width = 120, -- Width of the floating window
+                height = 25, -- Height of the floating window
+                default_mappings = false, -- Bind default mappings
+                debug = false, -- Print debug information
+                opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+                post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
                 -- You can use "default_mappings = true" setup option
                 -- Or explicitly set keybindings
-            }
+            })
             vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
             vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
             vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
-        end
+        end,
     },
     {
         "Pocco81/auto-save.nvim",
@@ -185,14 +172,71 @@ lvim.plugins = {
             })
         end,
     },
+    "ray-x/go.nvim",
+    "ray-x/guihua.lua",
+    {
+        "rcarriga/nvim-notify",
+        config = function()
+            vim.notify = require('notify')
+        end
+    },
+    {
+        "theHamsta/nvim-dap-virtual-text",
+        config = function()
+            require("nvim-dap-virtual-text").setup {
+                enabled = true, -- enable this plugin (the default)
+                enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+                highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+                highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+                show_stop_reason = true, -- show stop reason when stopped for exceptions
+                commented = false, -- prefix virtual text with comment string
+                only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+                all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+                --- A callback that determines how a variable is displayed or whether it should be omitted
+                --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+                --- @param buf number
+                --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+                --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+                --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+                display_callback = function(variable, _buf, _stackframe, _node)
+                    return variable.name .. ' = ' .. variable.value
+                end,
+
+                -- experimental features:
+                virt_text_pos = 'eol', -- position of virtual text, see `:h nvim_buf_set_extmark()`
+                all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+                virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+                virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
+                -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+            }
+        end
+    }
 }
+
+-- add your own keymapping
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- lvim.keys.normal_mode["<Left>"] = ":echo \"Use h\"<cr>"
+-- lvim.keys.normal_mode["<Right>"] = ":echo \"Use l\"<cr>"
+-- lvim.keys.normal_mode["<Up>"] = ":echo \"Use k\"<cr>"
+-- lvim.keys.normal_mode["<Down>"] = ":echo \"Use j\"<cr>"
+
+lvim.keys.insert_mode["<C-b>"] = "<Left>"
+lvim.keys.insert_mode["<C-f>"] = "<Right>"
+lvim.keys.insert_mode["<C-p>"] = "<Up>"
+lvim.keys.insert_mode["<C-n>"] = "<Down>"
+
+lvim.builtin.lualine.active = true
+lvim.builtin.dap.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.bufferline.active = true
+
 -- example mappings you can place in some other place
 -- An awesome method to jump to windows
-local picker = require('window-picker')
+local picker = require("window-picker")
 
 vim.keymap.set("n", ",w", function()
     local picked_window_id = picker.pick_window({
-        include_current_win = true
+        include_current_win = true,
     }) or vim.api.nvim_get_current_win()
     vim.api.nvim_set_current_win(picked_window_id)
 end, { desc = "Pick a window" })
@@ -200,7 +244,7 @@ end, { desc = "Pick a window" })
 -- Swap two windows using the awesome window picker
 local function swap_windows()
     local window = picker.pick_window({
-        include_current_win = false
+        include_current_win = false,
     })
     local target_buffer = vim.fn.winbufnr(window)
     -- Set the target window to contain current buffer
@@ -209,13 +253,101 @@ local function swap_windows()
     vim.api.nvim_win_set_buf(0, target_buffer)
 end
 
-vim.keymap.set('n', ',W', swap_windows, { desc = 'Swap windows' })
+require('go').setup({
+    disable_defaults = false, -- true|false when true set false to all boolean settings and replace all table
+    -- settings with {}
+    go = 'go', -- go command, can be go[default] or go1.18beta1
+    goimport = 'gopls', -- goimport command, can be gopls[default] or goimport
+    fillstruct = 'gopls', -- can be nil (use fillstruct, slower) and gopls
+    gofmt = 'gofumpt', --gofmt cmd,
+    max_line_len = 128, -- max line length in golines format, Target maximum line length for golines
+    tag_transform = false, -- can be transform option("snakecase", "camelcase", etc) check gomodifytags for details and more options
+    tag_options = 'json=omitempty', -- sets options sent to gomodifytags, i.e., json=omitempty
+    gotests_template = "", -- sets gotests -template parameter (check gotests for details)
+    gotests_template_dir = "", -- sets gotests -template_dir parameter (check gotests for details)
+    comment_placeholder = '', -- comment_placeholder your cool placeholder e.g. ﳑ       
+    icons = { breakpoint = '🧘', currentpos = '🏃' }, -- setup to `false` to disable icons setup
+    verbose = false, -- output loginf in messages
+    lsp_cfg = true, -- true: use non-default gopls setup specified in go/lsp.lua
+    -- false: do nothing
+    -- if lsp_cfg is a table, merge table with with non-default gopls setup in go/lsp.lua, e.g.
+    --   lsp_cfg = {settings={gopls={matcher='CaseInsensitive', ['local'] = 'your_local_module_path', gofumpt = true }}}
+    lsp_gofumpt = false, -- true: set default gofmt in gopls format to gofumpt
+    lsp_on_attach = true, -- nil: use on_attach function defined in go/lsp.lua,
+    --      when lsp_cfg is true
+    -- if lsp_on_attach is a function: use this function as on_attach function for gopls
+    lsp_keymaps = true, -- set to false to disable gopls/lsp keymap
+    lsp_codelens = true, -- set to false to disable codelens, true by default, you can use a function
+    -- function(bufnr)
+    --    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>F", "<cmd>lua vim.lsp.buf.formatting()<CR>", {noremap=true, silent=true})
+    -- end
+    -- to setup a table of codelens
+    lsp_diag_hdlr = true, -- hook lsp diag handler
+    lsp_diag_underline = true,
+    -- virtual text setup
+    lsp_diag_virtual_text = { space = 0, prefix = "" },
+    lsp_diag_signs = true,
+    lsp_diag_update_in_insert = false,
+    lsp_document_formatting = true,
+    -- set to true: use gopls to format
+    -- false if you want to use other formatter tool(e.g. efm, nulls)
+    lsp_inlay_hints = {
+        enable = true,
+        -- Only show inlay hints for the current line
+        only_current_line = false,
+        -- Event which triggers a refersh of the inlay hints.
+        -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+        -- not that this may cause higher CPU usage.
+        -- This option is only respected when only_current_line and
+        -- autoSetHints both are true.
+        only_current_line_autocmd = "CursorHold",
+        -- whether to show variable name before type hints with the inlay hints or not
+        -- default: false
+        show_variable_name = true,
+        -- prefix for parameter hints
+        parameter_hints_prefix = " ",
+        show_parameter_hints = true,
+        -- prefix for all the other hints (type, chaining)
+        other_hints_prefix = "=> ",
+        -- whether to align to the lenght of the longest line in the file
+        max_len_align = false,
+        -- padding from the left if max_len_align is true
+        max_len_align_padding = 1,
+        -- whether to align to the extreme right or not
+        right_align = false,
+        -- padding from the right if right_align is true
+        right_align_padding = 6,
+        -- The color of the hints
+        highlight = "Comment",
+    },
+    gopls_cmd = nil, -- if you need to specify gopls path and cmd, e.g {"/home/user/lsp/gopls", "-logfile","/var/log/gopls.log" }
+    gopls_remote_auto = true, -- add -remote=auto to gopls
+    gocoverage_sign = "█",
+    sign_priority = 5, -- change to a higher number to override other signs
+    dap_debug = true, -- set to false to disable dap
+    dap_debug_keymap = true, -- true: use keymap for debugger defined in go/dap.lua
+    -- false: do not use keymap in go/dap.lua.  you must define your own.
+    -- windows: use visual studio keymap
+    dap_debug_gui = {}, -- bool|table put your dap-ui setup here set to false to disable
+    dap_debug_vt = { enabled_commands = true, all_frames = true }, -- bool|table put your dap-virtual-text setup here set to false to disable
 
--- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+    dap_port = 38697, -- can be set to a number, if set to -1 go.nvim will pickup a random port
+    dap_timeout = 15, --  see dap option initialize_timeout_sec = 15,
+    dap_retries = 20, -- see dap option max_retries
+    build_tags = "tag1,tag2", -- set default build tags
+    textobjects = true, -- enable default text jobects through treesittter-text-objects
+    test_runner = 'go', -- one of {`go`, `richgo`, `dlv`, `ginkgo`, `gotestsum`}
+    verbose_tests = true, -- set to add verbose flag to tests deprecated, see '-v' option
+    run_in_floaterm = false, -- set to true to run in float window. :GoTermClose closes the floatterm
+    -- float term recommend if you use richgo/ginkgo with terminal color
+
+    floaterm = { -- position
+        posititon = 'auto', -- one of {`top`, `bottom`, `left`, `right`, `center`, `auto`}
+        width = 0.45, -- width of float window if not auto
+        height = 0.98, -- height of float window if not auto
+    },
+    trouble = false, -- true: use trouble to open quickfix
+    test_efm = false, -- errorfomat for quickfix, default mix mode, set to true will be efm only
+    luasnip = false, -- enable included luasnip snippets. you can also disable while add lua/snips folder to luasnip load
+    --  Do not enable this if you already added the path, that will duplicate the entries
+})
