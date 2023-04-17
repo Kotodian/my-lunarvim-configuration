9a07e682fd9657f47f0d9ba775df7b1c259e46bb
linqiankai <417350372@qq.com>
2023-04-17 22:54:30 +0800

    backgroud_colour to background_color

--- config.lua
+++ config.lua
@@ -400,7 +400,7 @@ lvim.plugins = {
         config = function()
             local notify = require('notify')
             notify.setup({
-                backgroud_colour = '#000000'
+                background_colour = '#000000'
             })
             vim.notify = function(msg, ...)
                 if msg:match("warning: multiple different client offset_encodings") then
