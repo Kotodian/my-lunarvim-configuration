8628013b959ee033ac7fcc40f46cd922865876ee
linqiankai <417350372@qq.com>
2023-04-23 21:19:30 +0800

    删除chatgpt以及lazy sync

--- config.lua
+++ config.lua
@@ -610,86 +610,6 @@ lvim.plugins = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     {
-        "jackMort/ChatGPT.nvim",
-        config = function()
-            require("chatgpt").setup({
-                -- optional configuration
-                -- welcome_message = WELCOME_MESSAGE, -- set to "" if you don't like the fancy godot robot
-                loading_text = "loading",
-                question_sign = "", -- you can use emoji if you want e.g. 🙂
-                answer_sign = "ﮧ", -- 🤖
-                max_line_length = 120,
-                yank_register = "+",
-                chat_layout = {
-                    relative = "editor",
-                    position = "50%",
-                    size = {
-                        height = "80%",
-                        width = "80%",
-                    },
-                },
-                settings_window = {
-                    border = {
-                        style = "rounded",
-                        text = {
-                            top = " Settings ",
-                        },
-                    },
-                },
-                chat_window = {
-                    filetype = "chatgpt",
-                    border = {
-                        highlight = "FloatBorder",
-                        style = "rounded",
-                        text = {
-                            top = " ChatGPT ",
-                        },
-                    },
-                },
-                chat_input = {
-                    prompt = "  ",
-                    border = {
-                        highlight = "FloatBorder",
-                        style = "rounded",
-                        text = {
-                            top_align = "center",
-                            top = " Prompt ",
-                        },
-                    },
-                },
-                openai_params = {
-                    model = "text-davinci-003",
-                    frequency_penalty = 0,
-                    presence_penalty = 0,
-                    max_tokens = 300,
-                    temperature = 0,
-                    top_p = 1,
-                    n = 1,
-                },
-                openai_edit_params = {
-                    model = "code-davinci-edit-001",
-                    temperature = 0,
-                    top_p = 1,
-                    n = 1,
-                },
-                keymaps = {
-                    close = { "<C-c>", "<Esc>" },
-                    yank_last = "<C-y>",
-                    scroll_up = "<C-u>",
-                    scroll_down = "<C-d>",
-                    toggle_settings = "<C-o>",
-                    new_session = "<C-n>",
-                    cycle_windows = "<Tab>",
-                },
-            })
-        end,
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim"
-        },
-    },
-    {
         "norcalli/nvim-colorizer.lua",
         config = function()
             require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
@@ -715,7 +635,6 @@ lvim.plugins = {
     },
 }
 
-require 'chatgpt'.setup()
 
 -- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.keys.normal_mode["<Left>"] = ":echo \"Use h\"<cr>"
