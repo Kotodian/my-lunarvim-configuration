3081b73b43727fd8314884849567c9b190e4cc5a
Kotodian <417350372@qq.com>
2024-05-23 15:26:51 +0800

    add codeium plugin

--- config.lua
+++ config.lua
@@ -751,7 +751,19 @@ lvim.plugins = {
                 },
             })
         end
-    }
+    },
+ -- should update lunarvim.builtin.cmp.sources
+ {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({
+        })
+    end
+},
 }
 
 
